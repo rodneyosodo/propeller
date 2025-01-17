087f9a2a3c7d94ab0b311f78696f52a5a78b64d4
Rodney Osodo <socials@rodneyosodo.com>
2025-01-17 10:25:54 +0300

    feat(cli): make cli interactive
    Signed-off-by: Rodney Osodo <socials@rodneyosodo.com>

--- cli/provision.go
+++ cli/provision.go
@@ -3,9 +3,12 @@ package cli
 import (
 	"fmt"
 	"os"
+	"strings"
 
+	"github.com/0x6flab/namegenerator"
 	smqSDK "github.com/absmach/magistrala/pkg/sdk/go"
 	"github.com/absmach/supermq/pkg/errors"
+	"github.com/charmbracelet/huh"
 	"github.com/spf13/cobra"
 )
 
@@ -16,7 +19,9 @@ var (
 	errFailedClientCreation     = errors.New("failed to create client")
 	errFailedConnectionCreation = errors.New("failed to create connection")
 
-	smqsdk smqSDK.SDK
+	smqsdk   smqSDK.SDK
+	namegen  = namegenerator.NewGenerator()
+	fileName = "config.toml"
 )
 
 const filePermission = 0o644
@@ -25,108 +30,176 @@ func SetSuperMQSDK(sdk smqSDK.SDK) {
 	smqsdk = sdk
 }
 
-type Result struct {
-	ManagerThing   smqSDK.Thing   `json:"manager_thing,omitempty"`
-	ManagerChannel smqSDK.Channel `json:"manager_channel,omitempty"`
-	PropletThing   smqSDK.Thing   `json:"proplet_thing,omitempty"`
-	PropletChannel smqSDK.Channel `json:"proplet_channel,omitempty"`
-}
-
 var provisionCmd = &cobra.Command{
 	Use:   "provision",
 	Short: "Provision resources",
 	Long:  `Provision necessary resources for Propeller operation.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		u := smqSDK.Login{
-			Identity: "admin@example.com",
-			Secret:   "12345678",
-		}
-		tkn, err := smqsdk.CreateToken(u)
-		if err != nil {
-			logErrorCmd(*cmd, errors.Wrap(errFailedToCreateToken, err))
+		var (
+			identity           string
+			secret             string
+			err                error
+			token              smqSDK.Token
+			domainName         string
+			domainAlias        string
+			domainPermission   string
+			domain             smqSDK.Domain
+			managerThingName   string
+			managerThing       smqSDK.Thing
+			propletThingName   string
+			propletThing       smqSDK.Thing
+			managerChannelName string
+			managerChannel     smqSDK.Channel
+		)
+		form := huh.NewForm(
+			huh.NewGroup(
+				huh.NewInput().
+					Title("Enter your identity (e-mail)?").
+					Value(&identity).
+					Validate(func(str string) error {
+						if str == "" {
+							return errors.New("identity is required")
+						}
 
-			return
-		}
-		logSuccessCmd(*cmd, "Successfully created access token")
+						return nil
+					}),
+				huh.NewInput().
+					Title("Enter your secret").
+					EchoMode(huh.EchoModePassword).
+					Value(&secret).
+					Validate(func(str string) error {
+						if str == "" {
+							return errors.New("secret is required")
+						}
+						u := smqSDK.Login{
+							Identity: identity,
+							Secret:   secret,
+						}
 
-		domain := smqSDK.Domain{
-			Name:       "demo",
-			Alias:      "demo",
-			Permission: "admin",
-		}
-		domain, err = smqsdk.CreateDomain(domain, tkn.AccessToken)
-		if err != nil {
-			logErrorCmd(*cmd, errors.Wrap(errFailedToCreateDomain, err))
+						token, err = smqsdk.CreateToken(u)
+						if err != nil {
+							return errors.Wrap(errFailedToCreateToken, err)
+						}
 
-			return
-		}
-		logSuccessCmd(*cmd, "Successfully created domain")
+						return nil
+					}),
+			),
+			huh.NewGroup(
+				huh.NewInput().
+					Title("Enter your domain name(leave empty to auto generate)").
+					Value(&domainName),
+				huh.NewInput().
+					Title("Enter your domain alias(leave empty to auto generate)").
+					Value(&domainAlias),
+				huh.NewSelect[string]().
+					Title("Select your domain permission").
+					Options(
+						huh.NewOption("admin", "admin"),
+						huh.NewOption("edit", "edit"),
+						huh.NewOption("view", "view"),
+					).
+					Value(&domainPermission).
+					Validate(func(str string) error {
+						if domainName == "" {
+							domainName = namegen.Generate()
+						}
+						if domainAlias == "" {
+							domainAlias = strings.ToLower(domainName)
+						}
+						domain = smqSDK.Domain{
+							Name:       domainName,
+							Alias:      domainAlias,
+							Permission: domainPermission,
+						}
+						domain, err = smqsdk.CreateDomain(domain, token.AccessToken)
+						if err != nil {
+							return errors.Wrap(errFailedToCreateDomain, err)
+						}
 
-		managerThing := smqSDK.Thing{
-			Name:   "Propeller Manager",
-			Tags:   []string{"manager", "propeller"},
-			Status: "enabled",
-		}
-		managerThing, err = smqsdk.CreateThing(managerThing, domain.ID, tkn.AccessToken)
-		if err != nil {
-			logErrorCmd(*cmd, errors.Wrap(errFailedClientCreation, err))
+						return nil
+					}),
+			),
+			huh.NewGroup(
+				huh.NewInput().
+					Title("Enter your manager thing name(leave empty to auto generate)").
+					Value(&managerThingName).
+					Validate(func(str string) error {
+						if str == "" {
+							managerThingName = namegen.Generate()
+						}
+						managerThing = smqSDK.Thing{
+							Name:   managerThingName,
+							Tags:   []string{"manager", "propeller"},
+							Status: "enabled",
+						}
+						managerThing, err = smqsdk.CreateThing(managerThing, domain.ID, token.AccessToken)
+						if err != nil {
+							return errors.Wrap(errFailedClientCreation, err)
+						}
 
-			return
-		}
-		logSuccessCmd(*cmd, "Successfully created manager client")
+						return nil
+					}),
+			),
+			huh.NewGroup(
+				huh.NewInput().
+					Title("Enter your proplet thing name(leave empty to auto generate)").
+					Value(&propletThingName).
+					Validate(func(str string) error {
+						if str == "" {
+							propletThingName = namegen.Generate()
+						}
+						propletThing = smqSDK.Thing{
+							Name:   propletThingName,
+							Tags:   []string{"proplet", "propeller"},
+							Status: "enabled",
+						}
+						propletThing, err = smqsdk.CreateThing(propletThing, domain.ID, token.AccessToken)
+						if err != nil {
+							return errors.Wrap(errFailedClientCreation, err)
+						}
 
-		propletThing := smqSDK.Thing{
-			Name:   "Propeller Proplet",
-			Tags:   []string{"proplet", "propeller"},
-			Status: "enabled",
-		}
-		propletThing, err = smqsdk.CreateThing(propletThing, domain.ID, tkn.AccessToken)
-		if err != nil {
-			logErrorCmd(*cmd, errors.Wrap(errFailedClientCreation, err))
+						return nil
+					}),
+			), huh.NewGroup(
+				huh.NewInput().
+					Title("Enter your manager channel name(leave empty to auto generate)").
+					Value(&managerChannelName).
+					Validate(func(str string) error {
+						if str == "" {
+							managerChannelName = namegen.Generate()
+						}
+						managerChannel = smqSDK.Channel{
+							Name:   managerChannelName,
+							Status: "enabled",
+						}
+						managerChannel, err = smqsdk.CreateChannel(managerChannel, domain.ID, token.AccessToken)
+						if err != nil {
+							return errors.Wrap(errFailedChannelCreation, err)
+						}
 
-			return
-		}
-		logSuccessCmd(*cmd, "Successfully created proplet client")
+						managerConns := smqSDK.Connection{
+							ThingID:   managerThing.ID,
+							ChannelID: managerChannel.ID,
+						}
+						if err = smqsdk.Connect(managerConns, domain.ID, token.AccessToken); err != nil {
+							return errors.Wrap(errFailedConnectionCreation, err)
+						}
 
-		managerChannel := smqSDK.Channel{
-			Name:   "Propeller Manager",
-			Status: "enabled",
-		}
-		managerChannel, err = smqsdk.CreateChannel(managerChannel, domain.ID, tkn.AccessToken)
-		if err != nil {
-			logErrorCmd(*cmd, errors.Wrap(errFailedChannelCreation, err))
+						propletConns := smqSDK.Connection{
+							ThingID:   propletThing.ID,
+							ChannelID: managerChannel.ID,
+						}
+						if err = smqsdk.Connect(propletConns, domain.ID, token.AccessToken); err != nil {
+							return errors.Wrap(errFailedConnectionCreation, err)
+						}
 
-			return
-		}
-		logSuccessCmd(*cmd, "Successfully created manager channel")
+						return nil
+					}),
+			),
+		)
 
-		managerConns := smqSDK.Connection{
-			ThingID:   managerThing.ID,
-			ChannelID: managerChannel.ID,
-		}
-		if err = smqsdk.Connect(managerConns, domain.ID, tkn.AccessToken); err != nil {
+		if err := form.Run(); err != nil {
 			logErrorCmd(*cmd, errors.Wrap(errFailedConnectionCreation, err))
-
-			return
-		}
-		logSuccessCmd(*cmd, "Successfully created manager connections")
-
-		propletConns := smqSDK.Connection{
-			ThingID:   propletThing.ID,
-			ChannelID: managerChannel.ID,
-		}
-		if err = smqsdk.Connect(propletConns, domain.ID, tkn.AccessToken); err != nil {
-			logErrorCmd(*cmd, errors.Wrap(errFailedConnectionCreation, err))
-
-			return
-		}
-		logSuccessCmd(*cmd, "Successfully created proplet connections")
-
-		res := Result{
-			ManagerThing:   managerThing,
-			ManagerChannel: managerChannel,
-			PropletThing:   propletThing,
-			PropletChannel: managerChannel,
 		}
 
 		configContent := fmt.Sprintf(`# SuperMQ Configuration
@@ -156,17 +229,24 @@ channel_id = "%s"`,
 			managerChannel.ID,
 		)
 
-		if err := os.WriteFile("config.toml", []byte(configContent), filePermission); err != nil {
-			logErrorCmd(*cmd, errors.New("failed to create config.toml file"))
+		if err := os.WriteFile(fileName, []byte(configContent), filePermission); err != nil {
+			logErrorCmd(*cmd, errors.New(fmt.Sprintf("failed to create %s file", fileName)))
 
 			return
 		}
-		logSuccessCmd(*cmd, "Successfully created config.toml file")
 
-		logJSONCmd(*cmd, res)
+		logSuccessCmd(*cmd, fmt.Sprintf("Successfully created %s file", fileName))
 	},
 }
 
 func NewProvisionCmd() *cobra.Command {
+	provisionCmd.PersistentFlags().StringVarP(
+		&fileName,
+		"file-name",
+		"f",
+		fileName,
+		"The name of the file to create",
+	)
+
 	return provisionCmd
 }
--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/absmach/magistrala v0.15.1
 	github.com/absmach/supermq v0.16.1-0.20250113091433-3a11b54394b2
 	github.com/caarlos0/env/v11 v11.3.1
+	github.com/charmbracelet/huh v0.6.0
 	github.com/eclipse/paho.mqtt.golang v1.5.0
 	github.com/fatih/color v1.18.0
 	github.com/go-chi/chi/v5 v5.2.0
@@ -22,6 +23,28 @@ require (
 	go.opentelemetry.io/otel v1.33.0
 	go.opentelemetry.io/otel/trace v1.33.0
 	golang.org/x/sync v0.10.0
+)
+
+require (
+	github.com/atotto/clipboard v0.1.4 // indirect
+	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
+	github.com/catppuccin/go v0.2.0 // indirect
+	github.com/charmbracelet/bubbles v0.20.0 // indirect
+	github.com/charmbracelet/bubbletea v1.2.4 // indirect
+	github.com/charmbracelet/lipgloss v1.0.0 // indirect
+	github.com/charmbracelet/x/ansi v0.4.5 // indirect
+	github.com/charmbracelet/x/exp/strings v0.0.0-20240722160745-212f7b056ed0 // indirect
+	github.com/charmbracelet/x/term v0.2.1 // indirect
+	github.com/dustin/go-humanize v1.0.1 // indirect
+	github.com/erikgeiser/coninput v0.0.0-20211004153227-1c3628e74d0f // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-localereader v0.0.1 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/mitchellh/hashstructure/v2 v2.0.2 // indirect
+	github.com/muesli/ansi v0.0.0-20230316100256-276c6243b2f6 // indirect
+	github.com/muesli/cancelreader v0.2.2 // indirect
+	github.com/muesli/termenv v0.15.3-0.20240618155329-98d742f6907a // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
 )
 
 require (
